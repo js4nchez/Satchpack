f77f91b0686c735d080c453461873ad8340de866
jerodriguez <jerodriguez_90@yahoo.com>
2012-09-11 21:55:08 -0600

    Updated the Sql Scripts
    Sql Scripts wouldn't run successfully. Inverted logic needed to happen.

---
+++ Satchpack/Satchpack.Database/Tables/CreateProductList.sql
@@ -0,0 +1,11 @@
+USE Satchpack
+GO
+
+IF NOT EXISTS (SELECT 1
+		   FROM INFORMATION_SCHEMA.TABLES
+		   WHERE TABLE_NAME = 'ProductList')
+CREATE TABLE ProductList (
+	ProductId INT NOT NULL REFERENCES Product(Id),
+	InvoiceId INT NOT NULL REFERENCES Invoice(Id)
+)
+GO
