081d629250f7a2f13983dbbbabd2917d4add684d
Ruddy Arroliga Jr. <rarroligajr@gmail.com>
2012-09-18 23:08:28 -0600

    Added message that tells user to organize lines.
    This is my 4th commit! :D I completed my goal!

--- Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
+++ Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
@@ -1,3 +1,12 @@
+@echo off
+echo ************************************************************
+echo *WHEN UPDATING BATCH FILE, ADD SCRIPTS THAT HAVE NO        *
+echo *REFERENCES TO THE TOP OF THE LIST, AND SCRIPTS THAT HAVE  *
+echo *REFERENCES TO THE BOTTOM OF THE LIST. IF YOU HAVE NOT DONE*
+echo *THIS, PLEASE CLOSE THIS FILE AND REORGANIZE IT            *
+echo ************************************************************
+pause
+
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Database\CreateDatabase.sql
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProduct.sql
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateShippingMethod.sql
