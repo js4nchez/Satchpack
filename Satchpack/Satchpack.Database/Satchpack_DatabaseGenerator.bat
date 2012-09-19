d8b1b412fe35a2ec33946bbe99fb8eebfa0fafc7
Ruddy Arroliga Jr. <rarroligajr@gmail.com>
2012-09-18 22:51:23 -0600

    Added 2 lines to add 2 new scripts
    Changed order for it all to work.

--- Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
+++ Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
@@ -1,8 +1,10 @@
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Database\CreateDatabase.sql
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProduct.sql
-sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateShippingMethod.sql
-sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateCustomer.sql
-sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProductList.sql
-sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInvoice.sql
-sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInventory.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateShippingMethod.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateCustomer.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInvoice.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProductList.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInventory.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateUser.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInvoiceStatus.sql
 pause
