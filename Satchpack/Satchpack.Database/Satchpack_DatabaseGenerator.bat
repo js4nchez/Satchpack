c1c3cfae46b572e2c68fadc60826b6053a659d9c
Ruddy Arroliga Jr. <rarroligajr@gmail.com>
2012-09-18 22:42:48 -0600

    Changed it from Ruddy to %UserName%

--- Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
+++ Satchpack/Satchpack.Database/Satchpack_DatabaseGenerator.bat
@@ -1,8 +1,8 @@
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Database\CreateDatabase.sql
 sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProduct.sql
-sqlcmd -S localhost -E -i C:\Users\Ruddy\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateShippingMethod.sql
-sqlcmd -S localhost -E -i C:\Users\Ruddy\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateCustomer.sql
-sqlcmd -S localhost -E -i C:\Users\Ruddy\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProductList.sql
-sqlcmd -S localhost -E -i C:\Users\Ruddy\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInvoice.sql
-sqlcmd -S localhost -E -i C:\Users\Ruddy\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInventory.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateShippingMethod.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateCustomer.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateProductList.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInvoice.sql
+sqlcmd -S localhost -E -i C:\Users\%UserName%\Documents\\GitHub\Satchpack\Satchpack\Satchpack.Database\Tables\CreateInventory.sql
 pause
