f079f56656f9529d9126d0982ddf675da591c295
jerodriguez <jerodriguez_90@yahoo.com>
2012-10-18 14:10:29 -0600

    Added PayPal functionality to pull customer info

--- Satchpack/Satchpack/Controllers/ShoppingController.cs
+++ Satchpack/Satchpack/Controllers/ShoppingController.cs
@@ -10,7 +10,6 @@ namespace Satchpack.Controllers
     public class ShoppingController : Controller
     {
         private PayPalAPIAAInterfaceClient sandboxApi;
-
         public ShoppingController()
         {
             sandboxApi = new PayPalAPIAAInterfaceClient();
@@ -28,14 +27,6 @@ namespace Satchpack.Controllers
 
         public ActionResult Checkout()
         {
-            CustomSecurityHeaderType securityHeader = new CustomSecurityHeaderType();
-            securityHeader.Credentials = new UserIdPasswordType()
-            {
-                Username = "bus_1350451002_biz_api1.satchpack.com",
-                Password = "1350451057",
-                Signature = "An5ns1Kso7MWUdW4ErQKJJJ4qi4-AAJIh61ld-z-TGnvds8gBpPq6JQU"
-            };
-
             SetExpressCheckoutRequestDetailsType sdt = new SetExpressCheckoutRequestDetailsType();
             PaymentDetailsType pdt = new PaymentDetailsType()
             {
@@ -47,17 +38,20 @@ namespace Satchpack.Controllers
                 }
             };
             sdt.PaymentDetails = new PaymentDetailsType[] { pdt };
-            sdt.ReturnURL = "http://localhost:25540/Shopping/EditCart";
+            sdt.ReturnURL = "http://localhost:25540/Shopping/Review";
             sdt.CancelURL = "http://localhost:25540/";
 
-            SetExpressCheckoutReq expressCheckout = new SetExpressCheckoutReq();
-            expressCheckout.SetExpressCheckoutRequest = new SetExpressCheckoutRequestType()
+            SetExpressCheckoutReq expressCheckout = new SetExpressCheckoutReq()
             {
-                SetExpressCheckoutRequestDetails = sdt,
-                Version = "60.0"
+                SetExpressCheckoutRequest = new SetExpressCheckoutRequestType()
+                {
+                    SetExpressCheckoutRequestDetails = sdt,
+                    Version = "60.0"
+                }
             };
 
-            SetExpressCheckoutResponseType response = sandboxApi.SetExpressCheckout(ref securityHeader, expressCheckout);
+            CustomSecurityHeaderType creds = RetrieveSecurityHeaders();
+            SetExpressCheckoutResponseType response = sandboxApi.SetExpressCheckout(ref creds, expressCheckout);
             if (response.Errors != null && response.Errors.Length > 0)
             {
                 throw new Exception("Exception while calling Paypal " + response.Errors[0].LongMessage);
@@ -66,11 +60,53 @@ namespace Satchpack.Controllers
             return Redirect("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=" + response.Token);
         }
 
-        public ActionResult Review()
+        public ActionResult Review(string token)
         {
-            //sandboxApi.GetExpressCheckoutDetails(
+            GetExpressCheckoutDetailsReq checkoutDetails = new GetExpressCheckoutDetailsReq();
+            checkoutDetails.GetExpressCheckoutDetailsRequest = new GetExpressCheckoutDetailsRequestType()
+            {
+                Token = token,
+                Version = "60.0"
+            };
+
+            CustomSecurityHeaderType creds = RetrieveSecurityHeaders();
+            GetExpressCheckoutDetailsResponseType response = sandboxApi.GetExpressCheckoutDetails(ref creds, checkoutDetails);
+            string payerId = response.GetExpressCheckoutDetailsResponseDetails.PayerInfo.PayerID;
 
             return View();
+        }
+
+        public ActionResult SubmitOrder(string token)
+        {
+            CustomSecurityHeaderType creds = RetrieveSecurityHeaders();
+            sandboxApi.DoExpressCheckoutPayment(ref creds, new DoExpressCheckoutPaymentReq()
+            {
+                DoExpressCheckoutPaymentRequest = new DoExpressCheckoutPaymentRequestType()
+                {
+                    DoExpressCheckoutPaymentRequestDetails = new DoExpressCheckoutPaymentRequestDetailsType()
+                    {
+                        Token = token
+                    }
+                }
+            });
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private CustomSecurityHeaderType RetrieveSecurityHeaders()
+        {
+            // Credentials for accessing paypal api.
+            CustomSecurityHeaderType securityHeader = new CustomSecurityHeaderType();
+            securityHeader.Credentials = new UserIdPasswordType()
+            {
+                Username = "bus_1350451002_biz_api1.satchpack.com",
+                Password = "1350451057",
+                Signature = "An5ns1Kso7MWUdW4ErQKJJJ4qi4-AAJIh61ld-z-TGnvds8gBpPq6JQU"
+            };
+            return securityHeader;
         }
     }
 }
