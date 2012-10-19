a35fb8f5c20baa4e68683accdba2795f76859262
jerodriguez <jerodriguez_90@yahoo.com>
2012-10-18 20:23:30 -0600

    Tidied up and commented the Product controller

--- Satchpack/Satchpack/Controllers/ShoppingController.cs
+++ Satchpack/Satchpack/Controllers/ShoppingController.cs
@@ -15,52 +15,62 @@ namespace Satchpack.Controllers
             sandboxApi = new PayPalAPIAAInterfaceClient();
         }
 
+        /// <summary>
+        /// Returns the view where a user can add products to their cart.
+        /// </summary>
         public ActionResult Product()
         {
             return View();
         }
 
+        /// <summary>
+        /// Returns the view where a user can modify their cart and proceed to checkout.
+        /// </summary>
         public ActionResult EditCart()
         {
             return View();
         }
 
+        /// <summary>
+        /// Initiates the PayPal checkout process.
+        /// Redirects the user to the PayPal site to retrieve customer info.
+        /// </summary>
         public ActionResult Checkout()
         {
-            SetExpressCheckoutRequestDetailsType sdt = new SetExpressCheckoutRequestDetailsType();
-            PaymentDetailsType pdt = new PaymentDetailsType()
-            {
-                OrderDescription = "Satchpack order",
-                OrderTotal = new BasicAmountType()
-                {
-                    currencyID = CurrencyCodeType.USD,
-                    Value = "90.00"
-                }
-            };
-            sdt.PaymentDetails = new PaymentDetailsType[] { pdt };
-            sdt.ReturnURL = "http://localhost:25540/Shopping/Review";
-            sdt.CancelURL = "http://localhost:25540/";
-
+            // This object holds the payment details.
             SetExpressCheckoutReq expressCheckout = new SetExpressCheckoutReq()
             {
                 SetExpressCheckoutRequest = new SetExpressCheckoutRequestType()
                 {
-                    SetExpressCheckoutRequestDetails = sdt,
+                    SetExpressCheckoutRequestDetails = new SetExpressCheckoutRequestDetailsType()
+                    {
+                        ReturnURL = "http://localhost:25540/Shopping/ReviewTransaction",
+                        CancelURL = "http://localhost:25540/Shopping/CancelTransaction",
+                        OrderDescription = "Satchpack Order",
+                        OrderTotal = new BasicAmountType()
+                        {
+                            currencyID = CurrencyCodeType.USD,
+                            Value = "90.00"
+                        }
+                    },
                     Version = "60.0"
                 }
             };
 
+            // Initiate the paypal checkout process and redirect to paypal
             CustomSecurityHeaderType creds = RetrieveSecurityHeaders();
             SetExpressCheckoutResponseType response = sandboxApi.SetExpressCheckout(ref creds, expressCheckout);
             if (response.Errors != null && response.Errors.Length > 0)
-            {
                 throw new Exception("Exception while calling Paypal " + response.Errors[0].LongMessage);
-            }
 
             return Redirect("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=" + response.Token);
         }
 
-        public ActionResult Review(string token)
+        /// <summary>
+        /// Receives the customer data and asks for confirmation from the user.
+        /// </summary>
+        /// <param name="token">The string that identifies the user's session for a transaction.</param>
+        public ActionResult ReviewTransaction(string token)
         {
             GetExpressCheckoutDetailsReq checkoutDetails = new GetExpressCheckoutDetailsReq();
             checkoutDetails.GetExpressCheckoutDetailsRequest = new GetExpressCheckoutDetailsRequestType()
@@ -76,10 +86,22 @@ namespace Satchpack.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Returns the view that assures the user of the cancellation of their order.
+        /// </summary>
+        public ActionResult CancelTransaction()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Submits the payment to PayPal as confirmed by the user.
+        /// </summary>
+        /// <param name="token">The string that identifies the user's session for a transaction.</param>
         public ActionResult SubmitOrder(string token)
         {
             CustomSecurityHeaderType creds = RetrieveSecurityHeaders();
-            sandboxApi.DoExpressCheckoutPayment(ref creds, new DoExpressCheckoutPaymentReq()
+            DoExpressCheckoutPaymentResponseType response = sandboxApi.DoExpressCheckoutPayment(ref creds, new DoExpressCheckoutPaymentReq()
             {
                 DoExpressCheckoutPaymentRequest = new DoExpressCheckoutPaymentRequestType()
                 {
@@ -93,12 +115,11 @@ namespace Satchpack.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Returns the neccessary credentials to make PayPal API calls.
         /// </summary>
-        /// <returns></returns>
         private CustomSecurityHeaderType RetrieveSecurityHeaders()
         {
-            // Credentials for accessing paypal api.
+            // Credentials for accessing paypal API.
             CustomSecurityHeaderType securityHeader = new CustomSecurityHeaderType();
             securityHeader.Credentials = new UserIdPasswordType()
             {
