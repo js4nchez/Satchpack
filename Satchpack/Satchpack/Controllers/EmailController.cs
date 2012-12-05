7e2bf93ac67bf0446cb784d754820557d107e673
jerodriguez <jerodriguez_90@yahoo.com>
2012-12-04 23:17:41 -0700

    Added email functionality

---
+++ Satchpack/Satchpack/Controllers/EmailController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+namespace Satchpack.Controllers
+{
+    public class EmailController : Controller
+    {
+        /// <summary>
+        /// Sends an email to the emails located in the 'dbo.DistributionEmails' table
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        private static void SendEmail(string from, string subject, string body)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(from);
+            message.Subject = subject;
+            message.Body = body;
+            message.To.Add(new MailAddress(ConfigurationManager.AppSettings["SupportEmail"]));
+            message.To.Add(new MailAddress(ConfigurationManager.AppSettings["PersonalEmail"]));
+
+            SmtpClient server = new SmtpClient(
+                ConfigurationManager.AppSettings["EmailHost"].ToString(),
+                int.Parse(ConfigurationManager.AppSettings["EmailPort"].ToString()));
+
+            server.Credentials = new NetworkCredential(
+                ConfigurationManager.AppSettings["SupportEmail"].ToString(),
+                ConfigurationManager.AppSettings["SupportEmailPassword"].ToString());
+            server.Send(message);
+        }
+    }
+}
